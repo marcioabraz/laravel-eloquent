d3d8773f77906c8a7a324bb0bd2859119d55c80c
= <=>
2022-02-01 22:00:18 -0300

    teste

--- app/Services/CalculadoraService.php
+++ app/Services/CalculadoraService.php
@@ -21,5 +21,33 @@ class CalculadoraService{
             'data' => $result
         ];
     }
+    public function div($num1, $num2)
+    {
+        try {
+            if ($num2==0){
+            return [
+                'success' => false,
+                'message' => 'Divisão por zero'
+            ];
+            }
+            $result = $num1/ $num2;
+   
+        } catch (\Throwable $th) {
+            return[
+                'success' => false,
+                'message' => 'Erro ao fazer div'
+            ];
+        }
+        return[
+            'success' => true,
+            'message' => 'Div feita com sucesso',
+            'data' => $result
+        ];
+        
+        
+       
+        
+
+    }
    
 }
