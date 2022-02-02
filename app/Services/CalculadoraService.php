aa6200d23a74c4833bdc3ed249112181b9db6343
= <=>
2022-02-01 21:44:12 -0300

    exer service

---
+++ app/Services/CalculadoraService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Services\CalculadoraService;
+
+class CalculadoraService{
+
+    public function sum($num1, $num2)
+    {
+        try {
+            $result = $num1 + $num2;
+        
+        } catch (\Throwable $th) {
+            return[
+                'success' => false,
+                'message' => 'Erro ao fazer soma'
+            ];
+        }
+        return[
+            'success' => true,
+            'message' => 'Soma feita com sucesso',
+            'data' => $result
+        ];
+    }
+   
+}
