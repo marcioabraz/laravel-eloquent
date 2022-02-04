e4785aa2280062869903ad7006d7c2268e8557d5
= <=>
2022-02-03 22:31:21 -0300

    teste

--- database/factories/BillFactory.php
+++ database/factories/BillFactory.php
@@ -14,12 +14,20 @@ class BillFactory extends Factory
     public function definition()
     {
         return [
-            'invoice'=> $this->faker->randomNumber(0,4),
+            'invoice'=> $this->faker->numberBetween(1000,999),
             'installment'=>$this->faker->randomDigit(),
             'value'=>$this->faker->randomFloat(0,0,100),
-            'client_id'=>$this->faker->randomNumber(),
+            'client_id'=>$this->faker->uuid,
             'due_date'=>$this->faker->dateTimeInInterval('','+1 weeks'),
             'payment_date'=>$this->faker->dateTimeInInterval('-1 week')
         ];
     }
 }
+
+
+// invoice: número de 4 dígitos aleatório
+// installment: número de 1 dígito aleatório
+// client_id: id de um cliente falso
+// value: número float menor que 100 aleatório
+// due_date: data entre 'now' e '+1 week'
+// payment_date: data entre '-1 week' e 'now'
