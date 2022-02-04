5deefb3eae12d37516fe80dbf2afad5ca7f8d773
= <=>
2022-02-03 22:45:49 -0300

    teste

--- database/factories/BillFactory.php
+++ database/factories/BillFactory.php
@@ -16,7 +16,7 @@ class BillFactory extends Factory
         return [
             'invoice'=> $this->faker->randomNumber(4,true),
             'installment'=>$this->faker->randomDigit(),
-            'client_id'=>$this->faker->numberBetween(0,1000),
+            'client_id'=>$this->faker->id,
             'value'=>$this->faker->randomFloat(0,0,100),
             'due_date'=>$this->faker->dateTimeBetween('now','+1 weeks'),
             'payment_date'=>$this->faker->dateTimeBetween('-1 week', 'now')
